855835ec48f92bfc825a88284b17218cb1867f7f
niu tech <jerzyglowacki@gmail.com>
2019-01-08 21:12:43 +0100

    Add sandbox to prevent frame busting

--- x-frame-bypass.js
+++ x-frame-bypass.js
@@ -5,11 +5,12 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 	connectedCallback () {
 		this.load(this.src)
 		this.src = ''
+		this.sandbox = '' + this.sandbox || 'allow-forms allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation' // all except allow-top-navigation
 	}
 	load (url, options) {
 		if (!url || !url.startsWith('http'))
-			throw new Error('src does not start with http(s)://')
-		console.log('Loading X-Frame-Bypass:', url)
+			throw new Error(`X-Frame-Bypass src ${url} does not start with http(s)://`)
+		console.log('X-Frame-Bypass loading:', url)
 		this.srcdoc = `<html>
 <head>
 	<style>
@@ -40,18 +41,18 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 </html>`
 		this.fetchProxy(url, options, 0).then(res => res.text()).then(data => {
 			if (data)
-				this.srcdoc = data.replace(/<head>/i, `<head>
+				this.srcdoc = data.replace(/<head([^>]*)>/i, `<head$1>
 	<base href="${url}">
 	<script>
 	// X-Frame-Bypass navigation event handlers
-	document.addEventListener("click", e => {
+	document.addEventListener('click', e => {
 		if (frameElement && document.activeElement && document.activeElement.href) {
 			e.preventDefault()
 			frameElement.load(document.activeElement.href)
 		}
 	})
-	document.addEventListener("submit", e => {
-		if (frameElement && document.activeElement && document.activeElement.form) {
+	document.addEventListener('submit', e => {
+		if (frameElement && document.activeElement && document.activeElement.form && document.activeElement.form.action) {
 			e.preventDefault()
 			if (document.activeElement.form.method === 'post')
 				frameElement.load(document.activeElement.form.action, {method: 'post', body: new FormData(document.activeElement.form)})
@@ -64,11 +65,15 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 	}
 	fetchProxy (url, options, i) {
 		const proxy = [
-			'https://cors-anywhere.herokuapp.com/',
 			'https://cors.io/?',
-			'https://jsonp.afeld.me/?url='
+			'https://jsonp.afeld.me/?url=',
+			'https://cors-anywhere.herokuapp.com/'
 		]
-		return fetch(proxy[i] + url, options).catch(error => {
+		return fetch(proxy[i] + url, options).then(res => {
+			if (!res.ok)
+				throw new Error(`${res.status} ${res.statusText}`);
+			return res
+		}).catch(error => {
 			if (i === proxy.length - 1)
 				throw error
 			return this.fetchProxy(url, options, i + 1)
