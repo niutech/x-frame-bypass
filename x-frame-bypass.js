114be374229146128a878fcb899e350e8a74ec37
Rob Gries <robert.w.gries@gmail.com>
2020-03-09 00:33:24 -0400

    Reload iframe when src attribute is changed

--- x-frame-bypass.js
+++ x-frame-bypass.js
@@ -1,6 +1,12 @@
 customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
+	static get observedAttributes() { return ['src'] }
 	constructor () {
 		super()
+	}
+	attributeChangedCallback () {
+		this.load(this.src)
+		this.src = ''
+		this.sandbox = '' + this.sandbox || 'allow-forms allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation' // all except allow-top-navigation
 	}
 	connectedCallback () {
 		this.load(this.src)
