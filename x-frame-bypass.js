c17fc970682ce331820ce5e3ddfd5a3d29e12b85
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-06-15 12:11:58 +0100

    Update to use more modern CORS proxies

--- x-frame-bypass.js
+++ x-frame-bypass.js
@@ -69,9 +69,7 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 	}
 	fetchProxy (url, options, i) {
 		const proxies = (options || {}).proxies || [
-			'https://cors-anywhere.herokuapp.com/',
-			'https://yacdn.org/proxy/',
-			'https://api.codetabs.com/v1/proxy/?quest='
+			'https://thingproxy.freeboard.io/fetch/'
 		]
 		return fetch(proxies[i] + url, options).then(res => {
 			if (!res.ok)
