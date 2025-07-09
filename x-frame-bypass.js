fe5cae62fd6a5a3737d923a0ac7535453fe4a48c
niu tech <jerzyglowacki@gmail.com>
2025-07-09 22:09:27 +0200

    Remove crossorigin

--- x-frame-bypass.js
+++ x-frame-bypass.js
@@ -12,10 +12,11 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 		this.sandbox = '' + this.sandbox || 'allow-forms allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation' // all except allow-top-navigation
 	}
 	load (url, options) {
-		if (!url || !url.startsWith('http'))
-			throw new Error(`X-Frame-Bypass src ${url} does not start with http(s)://`)
+		if (!url) return
+		if (!url.startsWith('http')) throw new Error(`X-Frame-Bypass src ${url} does not start with http(s)://`)
 		console.log('X-Frame-Bypass loading:', url)
-		this.srcdoc = `<html>
+		this.srcdoc = `<!DOCTYPE html>
+<html>
 <head>
 	<style>
 	.loader {
@@ -44,8 +45,7 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 </body>
 </html>`
 		this.fetchProxy(url, options, 0).then(res => res.text()).then(data => {
-			if (data)
-				this.srcdoc = data.replace(/<head([^>]*)>/i, `<head$1>
+			if (data) this.srcdoc = data.replace(/<head([^>]*)>/i, `<head$1>
 	<base href="${url}">
 	<script>
 	// X-Frame-Bypass navigation event handlers
@@ -58,13 +58,11 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 	document.addEventListener('submit', e => {
 		if (frameElement && document.activeElement && document.activeElement.form && document.activeElement.form.action) {
 			e.preventDefault()
-			if (document.activeElement.form.method === 'post')
-				frameElement.load(document.activeElement.form.action, {method: 'post', body: new FormData(document.activeElement.form)})
-			else
-				frameElement.load(document.activeElement.form.action + '?' + new URLSearchParams(new FormData(document.activeElement.form)))
+			if (document.activeElement.form.method === 'post') frameElement.load(document.activeElement.form.action, {method: 'post', body: new FormData(document.activeElement.form)})
+			else frameElement.load(document.activeElement.form.action + '?' + new URLSearchParams(new FormData(document.activeElement.form)))
 		}
 	})
-	</script>`)
+	</script>`).replace(/ crossorigin="[^"]*"/gi, '')
 		}).catch(e => console.error('Cannot load X-Frame-Bypass:', e))
 	}
 	fetchProxy (url, options, i) {
@@ -74,12 +72,10 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 			'https://api.codetabs.com/v1/proxy/?quest='
 		]
 		return fetch(proxies[i] + url, options).then(res => {
-			if (!res.ok)
-				throw new Error(`${res.status} ${res.statusText}`);
+			if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
 			return res
 		}).catch(error => {
-			if (i === proxies.length - 1)
-				throw error
+			if (i === proxies.length - 1) throw error
 			return this.fetchProxy(url, options, i + 1)
 		})
 	}
